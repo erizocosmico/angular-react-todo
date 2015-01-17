ad1b4d28b887409f3f47ecc84ef8405a4b1e3593
José Miguel Molina <hi@mvader.me>
2015-01-17 14:50:33 +0100

    Finished

---
+++ README.md
@@ -0,0 +1,7 @@
+# angular-react-todo
+
+This is an example TODO app built with Angular.js and React.js. It's only been developed for learning purposes.
+
+### TODO's
+* Test React components.
+* Test Angular controller.
--- src/js/views.jsx
+++ src/js/views.jsx
@@ -8,41 +8,59 @@
         changed: function (e) {
             this.setState({input: e.target.value});
         },
-        addTask: function () {
+        addTask: function (e) {
             this.props.scope.addTask(this.state.input);
             this.setState({input: ''}, function () {
                 this.refs.taskInput.getDOMNode().focus();
             });
+
+            e.preventDefault();
         },
         render: function () {
             return (
-                <div>
-                    <h2>Add a new task</h2>
-                    <input type='text' ref='taskInput' value={this.state.input} onChange={this.changed} />
-                    <button onClick={this.addTask}>Add</button>
-                </div>
+                <form id='task-form' onSubmit={this.addTask}>
+                    <input type='text' ref='taskInput' value={this.state.input} onChange={this.changed} placeholder='Add a new task...' />
+                </form>
             );
         }
     });
 
     window.TodoListItem = React.createClass({
+        getInitialState: function () {
+            return {input: this.props.task.title};
+        },
+        changed: function (e) {
+            this.setState({input: e.target.value});
+            this.props.scope.updateTitle(this.props.index, e.target.value);
+        },
+        remove: function () {
+            this.props.scope.removeTask(this.props.index);
+        },
         render: function () {
             return (
-                <li>{this.props.task.title}</li>
+                <li>
+                    <span className='title'>
+                        <input type='text' value={this.state.input} onChange={this.changed} />
+                    </span>
+                    <span className='time'>({this.props.task.added.toString()})</span>
+                    <span className='actions'>
+                        <button onClick={this.remove}>Completed</button>
+                    </span>
+                </li>
             );
         }
     });
 
     window.TodoList = React.createClass({
         render: function () {
-            var tasks = this.props.tasks.map(function (task) {
+            var tasks = this.props.tasks.map(function (task, i) {
                 return (
-                    <TodoListItem task={task} />
+                    <TodoListItem key={task.title + task.added} task={task} index={i} scope={this.props.scope} />
                 );
-            });
+            }.bind(this));
 
             return (
-                <ul>{tasks}</ul>
+                <ul id='task-list'>{tasks}</ul>
             );
         }
     });
